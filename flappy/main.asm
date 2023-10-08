067cd9af583a9b8a8aea1ca1185a0e7a12eb6e86
Simon Thoresen Hult <simon@hult-thoresen.com>
2023-10-08 11:46:26 +0200

    add funcs for acc, vel, pos

--- flappy/main.asm
+++ flappy/main.asm
@@ -29,35 +29,22 @@ main:
 
 main_loop:
     wait_for_frame()
-    draw_joy_state()
-
     inc _frame_count
 
-    lda _frame_count
-    and #%00001100
-    lsr
-    lsr
-    adc #$80
-    sta ADR_SPR0_POINTER
+    draw_joy_state()
+
+    read_player_acc()
+    calc_player_vel()
+    update_player_pos()
+
+    anim_player_spr()
+    draw_player_spr()
+
+    inc $d020
+    jmp main_loop
 
 
-    lda #MSK_JOY_RIGHT
-    bit ADR_JOY1_STATE
-    bne !+
-    add_val8($02, _player_pos_x)
-!:
-
-    lda #MSK_JOY_LEFT
-    bit ADR_JOY1_STATE
-    bne !+
-    sub_val8($02, _player_pos_x)
-!:
-
-
-    lda _player_pos_x+1
-    adc #'0'
-    sta $0400
-
+.macro draw_player_spr() {
     lda _player_pos_x
     sta ADR_SPR0_POSX
 
@@ -74,11 +61,60 @@ main_loop:
 
     lda _player_pos_y
     sta ADR_SPR0_POSY
+}
 
-    inc $d020
-    jmp main_loop
+.macro anim_player_spr() {
+    lda _frame_count
+    and #%00001100
+    lsr
+    lsr
+    adc #$80
+    sta ADR_SPR0_POINTER
+}
 
+.macro update_player_pos() {
+    lda _player_vel_x
+    cmp #$00
+    beq end
 
+    and #%10000000
+    cmp #$00
+    bne neg
+pos:
+    add_val8($02, _player_pos_x)
+    jmp end
+neg:
+    sub_val8($02, _player_pos_x)
+    // fall through
+end:
+}
+
+.macro calc_player_vel() {
+    lda _player_acc_x
+
+    // todo: smoothen
+
+    sta _player_vel_x
+}
+
+.macro read_player_acc() {
+    lda #$00
+    sta _player_acc_x
+
+    lda #MSK_JOY_RIGHT
+    bit ADR_JOY1_STATE
+    bne !+
+    lda #$01
+    sta _player_acc_x
+!:
+
+    lda #MSK_JOY_LEFT
+    bit ADR_JOY1_STATE
+    bne !+
+    lda #$81
+    sta _player_acc_x
+!:
+}
 
 .macro wait_for_frame() {
 !:  // in case the raster is on our marker line, we wait for it increment
