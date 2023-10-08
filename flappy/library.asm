84ee6397de465ff2d3bf5b6f1d4d3d1965021831
Simon Thoresen Hult <simon@hult-thoresen.com>
2023-10-08 04:43:01 +0200

    cleaned up constants as library

--- flappy/library.asm
+++ flappy/library.asm
@@ -8,21 +8,59 @@
 .const MSK_JOY_FIRE 			= $10
 
 .const COL_BLACK       			= $00
-.const COL_GREEN      			= $05
-.const COL_LGREEN				= $0d
+.const COL_WHITE       			= $01
+.const COL_RED       			= $02
+.const COL_CYAN       			= $03
+.const COL_PURPLE      			= $04
+.const COL_GREEN       			= $05
+.const COL_BLUE       			= $06
+.const COL_YELLOW       		= $07
+.const COL_ORANGE       		= $08
+.const COL_BROWN       			= $09
+.const COL_LRED       			= $0a
+.const COL_DGREY       			= $0b
+.const COL_GREY       			= $0c
+.const COL_LGREEN       		= $0d
+.const COL_LBLUE       			= $0e
+.const COL_LGREY       			= $0f
 
-.const ADR_SPRITE_POSX_BIT9     = $d010
+.const ADR_BORDER_COL        	= $d020
+.const ADR_BACKGROUND_COL    	= $d021
+.const ADR_SCREEN				= $0400	// default, 1000 bytes
+
 .const ADR_SPRITE_ENABLE 		= $d015
 .const ADR_SPRITE_MCOL_ENABLE	= $d01c
 .const ADR_SPRITE_MCOL0			= $d025
 .const ADR_SPRITE_MCOL1			= $d026
+.const ADR_SPRITE_POINTERS		= ADR_SCREEN + $03f8
+.const ADR_SPRITE_POSX_BIT9     = $d010
 
 .const ADR_SPR0_COLOR  		    = $d027
 .const ADR_SPR0_POSX 			= $d000
 .const ADR_SPR0_POSY 			= $d001
- 
-.const ADR_BORDER_COL        	= $d020
-.const ADR_BACKGROUND_COL    	= $d021
- 
-.const SPRITE_NUM_BYTES			= $40
+.const ADR_SPR0_POINTER			= ADR_SPRITE_POINTERS + 0
 
+.const SPRITE_NUM_BYTES			= $40 // 64 bytes
+
+
+
+.macro adc_8_to_16 (in8, in16, out16) {
+	lda in16
+	clc
+	adc in8
+	sta out16
+	lda in16+1
+	adc #$00
+	sta out16+1		
+}
+
+.macro adc_16 (in1, in2, out) {
+	lda in1
+	clc
+	adc in2
+	sta out
+
+	lda in1+1
+	adc in2+1
+	sta out+1
+}
--- flappy/main.asm
+++ flappy/main.asm
@@ -1,77 +1,29 @@
-
-/*
 main:
-    lda #SPRITE_0_BLOCK
-    sta ADR_SPRITE_0
+    lda #$82 // point to address of sprite in 64 multiples
+    sta ADR_SPR0_POINTER  
 
-    lda #%00000001
-    sta ADR_SPRITE_ENABLE // enable sprite 0, %0000 0001
+    lda #%00000001 
+    sta ADR_SPRITE_ENABLE
+    sta ADR_SPRITE_MCOL_ENABLE
 
-    lda #BLACK
-    sta ADR_ADR_SPR0_COLOR
-
-    ldx #$00
-build_sprite:
-    lda SPRITE_BUG,x 
-    sta ADR_SPR0_DATA,x
-    inx
-    cpx #$3f
-    bne build_sprite
-
-*/
-
-
-
- main:
-  lda #$0D              // using block 13 for sprite0
-  sta ADR_SPR0_INDEX  // set block 13 as target address for Data of Sprite0
- 
-  lda #%00000001              // enable...
-  sta ADR_SPRITE_ENABLE             // ...Sprite 0 => %0000 0001 (all sprites off except Sprite 0)
-  
-  lda #COL_BLACK      // load black color code into A
-  sta ADR_SPR0_COLOR    // make Sprite0 completely black
-  
-  // Reset Sprite Data
-  ldx #$00    // init x
-  lda #$00    // init a
-  
-clean:
-  sta ADR_SPR0_DATA,x   // write 0 into sprite data at x
-  inx                   // increment x
-  cpx #$7F              // is x <= 127?
-  bne clean             // if yes, goto clean
-  
-  // Build the Sprite
-  ldx #$00              // init x
-build:
-  lda SPRITE_BALL, x     // load data at x
-  sta ADR_SPR0_DATA,x   // write into sprite data at x
-  inx                   // increment x
-  cpx #$7F              // is x <= 127?
-  bne build             // if yes, goto build
-  
-  // Set Start Location of Sprite 0
-  ldx #$64                  // initial x position = 100
-  ldy #$46                  // initial y position =  70
-  stx ADR_SPR0_POSX   // move sprite 0 to x position
-  sty ADR_SPR0_POSY   // move sprite 0 to y position
-
-
-
-    lda #%11111111
-    sta ADR_SPRITE_MCOL_ENABLE // enable multicolor for all sprites
-
-    lda #$0f // sprite multicolor 1
+    lda #COL_BLACK
     sta ADR_SPRITE_MCOL0
-    lda #$02 // sprite multicolor 2
+    lda #COL_WHITE
     sta ADR_SPRITE_MCOL1
-    lda #COL_GREEN
+
+    lda #COL_LGREY
     sta ADR_SPR0_COLOR
 
+
+    lda #$64
+    sta ADR_SPR0_POSX
+    lda #$46
+    sta ADR_SPR0_POSY
+
+
 main_loop:
-    wait_for_frame()
-    draw_joy_state()
+    :wait_for_frame()
+    :draw_joy_state()
 
     inc _frame_count
 
@@ -79,17 +31,8 @@ main_loop:
     and #$04
     lsr
     lsr
-    adc #$0d
-    sta ADR_SPR0_INDEX
-
-/*
-    inc ADR_SPR0_INDEX
-    lda ADR_SPR0_INDEX
-    cmp #$0F
-    bne !+
-    lda #$0D              // using block 13 for sprite0   
-!:
-*/
+    adc #$82
+    sta ADR_SPR0_POINTER
 
     lda #MSK_JOY_RIGHT
     bit ADR_JOY1_STATE
