49cfc310349b74bd679cfda830ddb0268a3d92be
Simon Thoresen Hult <simon@hult-thoresen.com>
2023-10-27 22:53:19 +0200

    make stuff work

--- maptest/startup.asm
+++ maptest/startup.asm
@@ -2,13 +2,32 @@ BasicUpstart2(startup)
 #import "../c64lib/c64lib.asm"
 #import "data.asm"
 
+.const WALL_L = $0f
+.const WALL_R = $10
+.const WALL_UL = $77
+.const WALL_UC = $78
+.const WALL_UR = $79
+.const WALL_ML = $71
+.const WALL_MC = $72
+.const WALL_MR = $73
+.const WALL_BL = $74
+.const WALL_BC = $75
+.const WALL_BR = $76
+
+_scroll_x: 		.word $00
+_walls_data: 	.fill $40, $00 // 64 bytes cyclic buffer
+_walls_head:	.byte $00 // pointer to slot 0 in buffer
+_walls_seed: 	alloc_seed()
+
+
 *=$4000 "Main Program"
 startup:
 	enter_startup()
-	setup_irq($00, irq1)
+	//setup_irq($00, irq1)
 	leave_startup()
 
 main:
+/*
 	lda C64__SCREEN_CTRL2
 	ora #%00010000 // multicolor
 	sta C64__SCREEN_CTRL2
@@ -27,26 +46,11 @@ main:
 	ora #%00001010 // $2800-$2fff
 	sta C64__MEM_SETUP
 
-
-
-
-    ldx #$00
-!:
-.for (var i = 0; i < 4; i++) {
-	lda _screen + $0100*i,x
-    sta C64__SCREEN_DATA + $0100*i, x
-    tay
-    lda $3000,y // char color
-    and #$0f
-    sta C64__SCREEN_COLOR + $0100*i,x	
-}
-
-	inx    
-    bne !-
-
+	draw_level()
+*/
 main_loop:
-	// color border so that white means we use cpu, black means we idle
-	// for there to be smooth scrolling, the cpu needs to be released before 
+	// color border so that white means we use cpu, black means we idle for
+	// there to be smooth scrolling, the cpu needs to be released before 
 	// the rastering begins
 	lda #$00
 	sta C64__COLOR_BORDER
@@ -54,6 +58,8 @@ main_loop:
 	lda #$01
 	sta C64__COLOR_BORDER
 
+	// perform smooth scroll until we are ready to hard scroll the
+	// screen data
 	dec _scroll_x
 	lda _scroll_x
 	and #%00000111
@@ -62,72 +68,14 @@ main_loop:
 
 hard:
 	jsr scroll_walls
+	//1a: print_walls()
+	//1b: shift_screen()
 
-.for (var i = 1; i < 25; i++) {
 
-	lda C64__SCREEN_DATA + $0000 + 40*i
-	pha
-	ldx #$00
-!:
-	lda C64__SCREEN_DATA + $0001 + 40*i, x
-	sta C64__SCREEN_DATA + $0000 + 40*i, x
-    tay
-    lda $3000,y // char color
-    and #$0f
-    sta C64__SCREEN_COLOR + $0000 + 40*i,x	
 
-	inx
-	cpx #$27
-	bne !-
-	pla
-	sta C64__SCREEN_DATA + $0027 + 40*i
-    tay
-    lda $3000,y // char color
-    and #$0f
-    sta C64__SCREEN_COLOR + $0027 + 40*i,x	
 
-}
 	jmp main_loop
 
-_scroll_x: .word $00
-
-
-.macro print_walls()
-{
-	ldx _walls_head
-	ldy #$00
-!:
-	lda _walls_data, x
-	and #%00001111
-	clc 
-	adc #'0'
-	sta C64__SCREEN_DATA, y
-
-	lda _walls_data, x
-	lsr
-	lsr
-	lsr
-	lsr
-	clc 
-	adc #'0'
-	sta C64__SCREEN_DATA + $28, y
-
-	// increment wall buf ptr
-	inx
-	txa 
-	and #%00111111
-	tax
-
-	// increment screen pos
-	iny
-	cpy #$28
-	bne !-
-}
-
-
-_walls_data: .fill $40, $00 // 64 bytes cyclic buffer
-_walls_head: .byte $00 // pointer to slot 0 in buffer
-_walls_seed: alloc_seed()
 
 // extend current wall segment (bit 4+5)
 // bits 0-1; running counter of current segment (air or wall), value 0-3
@@ -167,15 +115,12 @@ extend_wall:
 	jmp !+
 create_wall:
 	lda_rand(_walls_seed)
-	sta C64__SCREEN_DATA + $50
 	// fall through	
 !:
 	and #%11110000 // keep upper nibble of wall
 	ora C64__ZEROP_BYTE // join with lower nibble counter
 	sta _walls_data, y
 	rts
-
-
 
 
 irq1:
@@ -193,7 +138,51 @@ irq2:
 	rti
 
 
-_screen:
+.macro shift_screen()
+{
+	.for (var i = 1; i < 25; i++) {
+
+		lda C64__SCREEN_DATA + $0000 + 40*i
+		pha
+		ldx #$00
+	!:
+		lda C64__SCREEN_DATA + $0001 + 40*i, x
+		sta C64__SCREEN_DATA + $0000 + 40*i, x
+		tay
+		lda $3000,y // char color
+		and #$0f
+		sta C64__SCREEN_COLOR + $0000 + 40*i,x	
+
+		inx
+		cpx #$27
+		bne !-
+		pla
+		sta C64__SCREEN_DATA + $0027 + 40*i
+		tay
+		lda $3000,y // char color
+		and #$0f
+		sta C64__SCREEN_COLOR + $0027 + 40*i,x	
+	}
+}
+
+.macro draw_level()
+{
+   ldx #$00
+!:
+.for (var i = 0; i < 4; i++) {
+	lda _level + $0100*i,x
+    sta C64__SCREEN_DATA + $0100*i, x
+    tay
+    lda $3000,y // char color
+    and #$0f
+    sta C64__SCREEN_COLOR + $0100*i,x	
+}
+
+	inx    
+    bne !-
+}
+
+_level:
 	.byte $0f,$10,$0f,$10,$0f,$10,$0f,$10,$0f,$10,$0f,$10,$0f,$10,$0f,$10,$0f,$10,$0f,$10,$0f,$10,$0f,$10,$0f,$10,$0f,$10,$0f,$10,$0f,$10,$0f,$10,$0f,$10,$0f,$10,$0f,$10
 	.byte $00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
 	.byte $00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
@@ -220,16 +209,35 @@ _screen:
 	.byte $00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$74,$75,$76,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
 	.byte $0f,$10,$0f,$10,$0f,$10,$0f,$10,$0f,$10,$0f,$10,$0f,$10,$0f,$10,$0f,$10,$0f,$10,$0f,$10,$0f,$10,$0f,$10,$0f,$10,$0f,$10,$0f,$10,$0f,$10,$0f,$10,$0f,$10,$0f,$10
 
-.const CHAR_BEAM_L = $0f
-.const CHAR_BEAM_R = $10
-.const CHAT_BOX_UL = $77
-.const CHAT_BOX_UC = $78
-.const CHAT_BOX_UR = $79
-.const CHAT_BOX_ML = $71
-.const CHAT_BOX_MC = $72
-.const CHAT_BOX_MR = $73
-.const CHAT_BOX_BL = $74
-.const CHAT_BOX_BC = $75
-.const CHAT_BOX_BR = $76
 
+.macro print_walls()
+{
+	ldx _walls_head
+	ldy #$00
+!:
+	lda _walls_data, x
+	and #%00001111
+	clc 
+	adc #'0'
+	sta C64__SCREEN_DATA, y
 
+	lda _walls_data, x
+	lsr
+	lsr
+	lsr
+	lsr
+	clc 
+	adc #'0'
+	sta C64__SCREEN_DATA + $28, y
+
+	// increment wall buf ptr
+	inx
+	txa 
+	and #%00111111
+	tax
+
+	// increment screen pos
+	iny
+	cpy #$28
+	bne !-
+}
