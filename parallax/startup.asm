1617b11b4a1fcc071b5416f66e65c16274d1c7cb
Simon Thoresen Hult <simon@hult-thoresen.com>
2023-11-08 15:29:12 +0100

    implement screen scroll + tile shift

--- parallax/startup.asm
+++ parallax/startup.asm
@@ -6,30 +6,48 @@ BasicUpstart2(startup)
 
 .label ADR_CHARS = $2800
 *=ADR_CHARS "Character Set"
+.byte %00000000
+.byte %00000000
+.byte %00000000
+.byte %00000000
+.byte %00000000
+.byte %00000000
+.byte %00000000
+.byte %00000000
+
+.byte %11111111
+.byte %11111111
+.byte %11111111
+.byte %11100111
+.byte %11100111
+.byte %11111111
+.byte %11111111
+.byte %11111111
+
 .byte %11111111
 .byte %10000000
 .byte %10000000
 .byte %10000000
-.byte %10100000
-.byte %10100000
-.byte %10100000
-.byte %10100000
+.byte %10000000
+.byte %10000000
+.byte %10000000
+.byte %10000000
 
 .byte %11111111
 .byte %00000001
 .byte %00000001
-.byte %00000101
-.byte %00000101
-.byte %00000101
-.byte %00000101
-.byte %00000101
+.byte %00000001
+.byte %00000001
+.byte %00000001
+.byte %00000001
+.byte %00000001
 
 .byte %10000000
 .byte %10000000
 .byte %10000000
 .byte %10000000
 .byte %10000000
-.byte %10111111
+.byte %10000000
 .byte %10000000
 .byte %11111111
 
@@ -38,28 +56,78 @@ BasicUpstart2(startup)
 .byte %00000001
 .byte %00000001
 .byte %00000001
-.byte %11111101
+.byte %00000001
 .byte %00000001
 .byte %11111111
 
-.fill $0800 - $04, $00
 
 *=$4000 "Main Program"
 #import "../c64lib/c64lib.asm"
 
+_world_x: 
+    .byte $00
 _screen:
     .for (var y = 0; y < 8; y++) {
         .for (var x = 0; x < 40; x++) {
-            .byte mod(x, 2) + mod(y, 2) * 2
+            .if (x >= 38 && y > 0 && y < 3) {
+                .byte $01
+            } else {
+                .byte $02 + mod(x, 2) + mod(y, 2) * 2
+            }
         }
     }
 
 startup:
-    jsr swap_charset
-
     lda #$00
     sta C64__COLOR_BORDER
 
+    clear_screen($00)
+    jsr swap_charset
+    jsr render_world
+
+main:
+    lda #$00
+    sta C64__COLOR_BORDER
+    wait_vblank()
+    lda #$02
+    inc C64__COLOR_BORDER
+
+    lda _world_x
+    and #$01
+    cmp #$01
+    bne !+
+    jsr rotate_tile_right
+!:
+
+    inc _world_x
+	lda _world_x
+	eor #$ff
+	sta C64__ZEROP_BYTE
+	scroll_screen_x_a8(C64__ZEROP_BYTE)
+
+    lda _world_x
+    and #$07
+    cmp #$00
+    bne main
+
+hard_scroll:
+    .for (var i = 0; i < 4; i++) {
+        lda C64__SCREEN_DATA + i * $28 + $00
+        pha
+        ldx #$00
+!:
+        lda C64__SCREEN_DATA + i * $28 + $01, x
+        sta C64__SCREEN_DATA + i * $28 + $00, x
+        inx
+        cpx #$27
+        bne !-
+        pla
+        sta C64__SCREEN_DATA + i * $28 + $27
+    }
+    jmp main
+
+
+render_world:
     ldx #$00
 !:
     lda _screen, x
@@ -67,20 +135,7 @@ startup:
     inx
     cpx #$a0
     bne !-
-
-main:
-    wait_vblank()
-    inc C64__COLOR_BORDER
-    jsr scroll_left
-
-    dec C64__COLOR_BORDER
-    jmp main
-
-
-.const charA = ADR_CHARS + $00
-.const charB = ADR_CHARS + $08
-.const charC = ADR_CHARS + $10
-.const charD = ADR_CHARS + $18
+    rts
 
 swap_charset:
 	lda C64__MEM_SETUP
@@ -89,22 +144,15 @@ swap_charset:
 	sta C64__MEM_SETUP
     rts
 
-copy_charset:
-    ldx #$00
-!:
-    .for (var i = 0; i < 8; i++) {
-        lda $d000 + i * $0100, x
-        sta ADR_CHARS + i * $0100, x
-    }
-    inx
-    bne !-
-    rts
 
+.const charA = ADR_CHARS + $10
+.const charB = ADR_CHARS + $18
+.const charC = ADR_CHARS + $20
+.const charD = ADR_CHARS + $28
 
-scroll_left:
+rotate_tile_left:
     ldx #$00
-loop1:		
-    lda charB,x		// bit7 charB -> carry
+!:  lda charB,x		// bit7 charB -> carry
     asl				
     rol charA,x		// carry -> bit0 charA
     rol charB,x		// bit7 charA -> bit0 charB
@@ -114,5 +162,90 @@ loop1:
     rol charC,x
     inx
     cpx #$08
-    bne loop1
+    bne !-
+    rts
+
+rotate_tile_right:
+    ldx #$00
+!:  lda charA,x		// bit0 charA -> carry
+    lsr				
+    ror charB,x		// carry -> bit7 charB
+    ror charA,x		// bit0 charB -> bit7 charA
+    lda charC,x		// same here with C and D
+    lsr
+    ror charD,x
+    ror charC,x
+    inx
+    cpx #$08
+    bne !-
+    rts
+
+rotate_tile_up:
+    lda charA		// save byte0 charA	
+    pha
+    lda charC		// save byte0 charC
+    pha
+    ldx #$00
+!:  lda charA+1,x
+    sta charA,x
+    lda charC+1,x
+    sta charC,x
+    inx
+    cpx #$07
+    bne !-
+    pla
+    sta charA+7		// byte0 charC -> byte7 charA
+    pla
+    sta charC+7		// byte0 charA -> byte7 charC		
+        
+    lda charB		// same here with B and D	
+    pha
+    lda charD
+    pha
+    ldx #$00
+!:  lda charB+1,x
+    sta charB,x
+    lda charD+1,x
+    sta charD,x
+    inx
+    cpx #$07
+    bne !-
+    pla
+    sta charB+7
+    pla
+    sta charD+7				
+    rts
+
+rotate_tile_down:
+    lda charC+7			
+    pha
+    lda charA+7
+    pha
+    ldx #$06
+!:  lda charC,x
+    sta charC+1,x
+    lda charA,x
+    sta charA+1,x
+    dex
+    bpl !-
+    pla
+    sta charC
+    pla
+    sta charA	
+
+    lda charD+7			
+    pha
+    lda charB+7
+    pha
+    ldx #$06
+!:  lda charD,x
+    sta charD+1,x
+    lda charB,x
+    sta charB+1,x
+    dex
+    bpl !-
+    pla
+    sta charD
+    pla
+    sta charB			
     rts
